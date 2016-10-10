f0fc21fa77ec639dc0553dce2f23286e455e4dd4
yefangyong <1281074511@qq.com>
2016-10-10 21:29:14 +0800

    guest2

---
+++ Application/Admin/Controller/ClassificationController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class ClassificationController extends CommonController {
+    public function index() {
+        $this->display();
+    }
+}
+
+?>

7238e3455d632837573eed14fb304e7b8e2759ed
yefangyong <1281074511@qq.com>
2016-10-10 22:53:10 +0800

    guest2

--- Application/Admin/Controller/ClassificationController.class.php
+++ Application/Admin/Controller/ClassificationController.class.php
@@ -3,7 +3,62 @@ namespace Admin\Controller;
 use Think\Controller;
 class ClassificationController extends CommonController {
     public function index() {
+        $page=$_REQUEST['p']?$_REQUEST['p']:1;
+        $pagesize=$_REQUEST['pagesize']?$_REQUEST['pagesize']:3;
+        $rel=D('Classification')->getClassification($page,$pagesize);
+        $Count=D('Classification')->getClassificationCount();
+        $res=new \Think\Page($Count,$pagesize);
+        $pageRes=$res->show();
+        $this->assign('page',$pageRes);
+        $this->assign('list',$rel);
         $this->display();
+    }
+
+    public function add() {
+        if($_POST) {
+            if(!$_POST['designation'] || !isset($_POST['designation'])) {
+                return show(0,'分类名称不得为空');
+            }
+            if($_POST['id']) {
+               return  $this->save($_POST);
+            }
+            $rel = D('classification')->insert($_POST);
+            if($rel) {
+                return show(1,'新增成功!');
+            }else{
+                return show(0,'新增失败!');
+            }
+        }else{
+            $this->display();
+        }
+    }
+
+    public function edit() {
+        $id = $_GET['id'];
+        $rel = D('Classification')->getClassificationByid($id);
+        $this->assign('list',$rel);
+        $this->display();
+    }
+
+    public function save($data=array()) {
+        $id = $data['id'];
+        unset($data['id']);
+        $rel = D('Classification')->UpdateClassificationById($id,$data);
+        if($rel) {
+            return show(1,'更新成功!');
+        }else{
+            return show(0,'更新失败!');
+        }
+    }
+
+    public function delete() {
+        $id = $_POST['id'];
+        $rel = D('Classification')->deleteClassification($id);
+        if($rel) {
+            return show(1,'删除成功!');
+        }else{
+            return show(0,'删除失败!');
+        }
     }
 }
 
---
+++ Application/Admin/Model/ClassificationModel.class.php
@@ -0,0 +1,36 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class ClassificationModel extends Model {
+    private $_db = '';
+    public function __construct() {
+        $this->_db = M('classification');
+    }
+
+    public function getClassification($page,$pagesize) {
+        //分页功能
+        $offset = ($page - 1) * $pagesize;
+        $list = $this->_db->order('id desc')->limit($offset, $pagesize)->select();
+        return $list;
+    }
+
+    public function getClassificationCount() {
+        return $this->_db->count();
+    }
+
+    public function insert($data=array()) {
+        return $this->_db->add($data);
+    }
+
+    public function getClassificationByid($id) {
+        return $this->_db->where('id='.$id)->select();
+    }
+
+    public function UpdateClassificationById($id,$data) {
+        return $this->_db->where('id='.$id)->save($data);
+    }
+
+    public function deleteClassification($id) {
+        return $this->_db->where('id='.$id)->delete();
+    }
+}
